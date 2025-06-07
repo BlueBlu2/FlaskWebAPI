208ae43df57318f712ee12764371fe29f211acbf
abdalrhmn <a@a.com>
2025-06-07 22:12:28 +0300

    route test

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,3 +1,22 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+stores = [
+    {
+        'name':'My store',
+        'items':[
+            {
+                'item_name':"Chair",
+                'item_price':15.99
+            }
+        ]
+    }
+]
+@app.get('/')
+def hello_app():
+    return "Hello from app"
+
+@app.get('/store')
+def get_stores():
+    return {"stores":stores}
