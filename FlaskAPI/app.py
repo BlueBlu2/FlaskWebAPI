0c6ce17a9b323a5e0bf411986d6a422e86a90f21
abdalrhmn <a@a.com>
2025-06-09 04:13:19 +0300

    Store items added

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,10 @@ stores = [
         ]
     }
 ]
+
+def get_request():
+    return request.get_json()
+    
 @app.get('/')
 def hello_app():
     return "Hello from app"
@@ -20,3 +24,23 @@ def hello_app():
 @app.get('/store')
 def get_stores():
     return {"stores":stores}
+
+@app.post('/store')
+def create_store():
+    request_data = request.get_json()
+    new_store  = {'name': request_data['name'], 'items':[]}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post('/store/<string:name>/item')
+def create_item(name):
+    data = get_request()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {
+                "name":data['name'],
+                "price":data['price']
+            }
+            store['items'].append(new_item)
+            return new_item, 201
+    return {"message":"Store not found"}, 404
