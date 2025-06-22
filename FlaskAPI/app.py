173b8d8f55d7b60978959b1a2257cefb240de2d5
Blue <eng.abdalrhmn.mohamed@gmail.com>
2025-06-22 13:29:30 +0300

    tags added only on stores

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -3,6 +3,7 @@ from flask_smorest import Api
 import os
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 from db import db
 import models
@@ -31,5 +32,7 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    
     
     return app
