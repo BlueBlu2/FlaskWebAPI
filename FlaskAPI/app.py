b97e2c419f19b7bf2a421fc269c26dfbd312ab55
Blue <eng.abdalrhmn.mohamed@gmail.com>
2025-06-26 02:10:12 +0300

    Tags for items added and initial setup for jwt added

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_smorest import Api
 import os
+import secrets
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from flask_jwt_extended import JWTManager
 
 from db import db
 import models
@@ -26,7 +28,13 @@ def create_app(db_url=None):
     db.init_app(app)
     
     api = Api(app)
-
+    
+    #app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128)
+    app.config["JWT_SECRET_KEY"] = "86630177918271227258918569529966116806"
+    jwt = JWTManager(app)
+    
+     
+    
     with app.app_context():
         db.create_all()
 
